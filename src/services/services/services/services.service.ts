f7110699dcdd7f5bc2f66b74d7da11726e1dad94
Neteree <neteree@gmail.com>
2024-09-27 19:56:58 +1200

    Added more services and updated the existing ones that we can provide

--- src/services/services/services/services.service.ts
+++ src/services/services/services/services.service.ts
@@ -4,25 +4,53 @@ import { Injectable } from '@nestjs/common';
 export class ServicesService {
   private services = [
     {
-      name: 'Website Front-end',
+      name: 'Website Front-end Coding',
       description:
-        'With this option we will provide you with a front-end of a website',
+        'With this option we will provide you with a front-end of a website, written using HTML, CSS, Tailwind, plain JavaScript and/or React',
       imgURL: 'An image url for this service',
       price: 750,
     },
     {
-      name: 'Website back-end',
+      name: 'Website Front-end UI/UX Design',
       description:
-        'With this option we will provide you with a back-end of a website',
+        'With this option we will provide you with front-end UI/UX Design of a website',
+      imgURL: 'An image url for this service',
+      price: 800,
+    },
+    {
+      name: 'Website SEO',
+      description:
+        'With this option we will improve the SEO of an existing website',
+      imgURL: 'An image url for this service',
+      price: 400,
+    },
+    {
+      name: 'Website API',
+      description:
+        'With this option we will provide you with a web API written in Express, NestJS, or ASP.NET',
       imgURL: 'An image url for this service',
       price: 1250,
     },
     {
-      name: 'Website front-end + back-end',
+      name: 'Database Integration',
       description:
-        'With this option we will provide you with both a front-end and a back-end of a website',
+        'With this option we will connect an existing API to a database',
       imgURL: 'An image url for this service',
-      price: 1500,
+      price: 500,
+    },
+    {
+      name: 'Unit Tests',
+      description:
+        'With this option we will create unit tests for either the backend and/or the front-end of your website using Jest, or XUnit',
+      imgURL: 'An image url for this service',
+      price: 1100,
+    },
+    {
+      name: 'Authorisation and Authentication',
+      description:
+        'With this option we will set up your site with Authorisation and Authentications',
+      imgURL: 'An image url for this service',
+      price: 2000,
     },
   ];
 
